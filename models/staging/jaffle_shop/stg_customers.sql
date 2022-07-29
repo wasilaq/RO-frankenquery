a7f82f999834165962b1f56606b4e0d023a9163d
Wasila Quader <wasila.quader@fishtownanalytics.com>
2022-07-29 13:55:35 +0000

    set up staging models

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,19 @@
+
+with source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
